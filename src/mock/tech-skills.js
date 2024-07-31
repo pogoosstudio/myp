bfc2e499be015f0f66f17e13a03024f85cc5688c
pogoosstudio <dioverrdm@gmail.com>
2024-07-31 19:59:45 -0400

    Update tech-skills.js

--- src/mock/tech-skills.js
+++ src/mock/tech-skills.js
@@ -1,13 +1,13 @@
 export const FRONTEND_SKILLS = [
   {
     label: 'HTML',
-    proficiency: '80%',
+    proficiency: '90%',
     icon: 'vscode-icons:file-type-html',
     iconClasses: 'text-3xl',
   },
   {
     label: 'CSS',
-    proficiency: '80%',
+    proficiency: '90%',
     icon: 'vscode-icons:file-type-css',
     iconClasses: 'text-3xl',
   },
@@ -19,7 +19,7 @@ export const FRONTEND_SKILLS = [
   },
   {
     label: 'Typescript',
-    proficiency: '80%',
+    proficiency: '50%',
     icon: 'logos:typescript-icon',
     iconClasses: 'text-2xl',
   },
@@ -31,75 +31,27 @@ export const FRONTEND_SKILLS = [
   },
   {
     label: 'Next.js',
-    proficiency: '90%',
+    proficiency: '80%',
     icon: 'cib:next-js',
     iconClasses: 'text-3xl',
   },
   {
-    label: 'Gatsby',
-    proficiency: '90%',
-    icon: 'logos:gatsby',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'Material UI (mui)',
-    proficiency: '90%',
-    icon: 'logos:material-ui',
-    iconClasses: 'text-2xl',
-  },
-  {
     label: 'Tailwind CSS',
-    proficiency: '90%',
+    proficiency: '50%',
     icon: 'logos:tailwindcss-icon',
     iconClasses: 'text-2xl',
   },
   {
     label: 'SCSS',
-    proficiency: '90%',
+    proficiency: '40%',
     icon: 'vscode-icons:file-type-scss2',
     iconClasses: 'text-3xl',
   },
   {
     label: 'Bootstrap',
-    proficiency: '90%',
+    proficiency: '50%',
     icon: 'logos:bootstrap',
     iconClasses: 'text-2xl',
-  },
-  {
-    label: 'Styled Components',
-    proficiency: '90%',
-    icon: 'simple-icons:styled-components',
-    iconClasses: 'text-4xl',
-  },
-  {
-    label: 'Redux',
-    proficiency: '90%',
-    icon: 'logos:redux',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'React Router',
-    proficiency: '90%',
-    icon: 'logos:react-router',
-    iconClasses: 'text-xl',
-  },
-  {
-    label: 'React Query',
-    proficiency: '90%',
-    icon: 'logos:react-query-icon',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'React Hook Form',
-    proficiency: '90%',
-    icon: 'fluent:form-20-regular',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'Lodash',
-    proficiency: '90%',
-    icon: 'logos:lodash',
-    iconClasses: 'text-xl',
   },
 ];
 
@@ -123,32 +75,8 @@ export const BACKEND_SKILLS = [
     iconClasses: 'text-3xl',
   },
   {
-    label: 'Graphql',
-    proficiency: '90%',
-    icon: 'logos:graphql',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'Prisma',
-    proficiency: '90%',
-    icon: 'vscode-icons:file-type-light-prisma',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'Sequelize',
-    proficiency: '90%',
-    icon: 'vscode-icons:file-type-sequelize',
-    iconClasses: 'text-3xl',
-  },
-  {
-    label: 'PostgreSQL',
-    proficiency: '90%',
-    icon: 'vscode-icons:file-type-pgsql',
-    iconClasses: 'text-3xl',
-  },
-  {
     label: 'MySQL',
-    proficiency: '90%',
+    proficiency: '60%',
     icon: 'logos:mysql',
     iconClasses: 'text-2xl',
   },
@@ -160,7 +88,7 @@ export const BACKEND_SKILLS = [
   },
   {
     label: 'AWS',
-    proficiency: '80%',
+    proficiency: '50%',
     icon: 'logos:aws',
     iconClasses: 'text-xl',
   },
@@ -171,20 +99,14 @@ export const BACKEND_SKILLS = [
     iconClasses: 'text-3xl',
   },
   {
-    label: 'Bitbucket',
-    proficiency: '80%',
-    icon: 'logos:bitbucket',
-    iconClasses: 'text-2xl',
-  },
-  {
     label: 'Docker',
-    proficiency: '80%',
+    proficiency: '50%',
     icon: 'logos:docker-icon',
     iconClasses: 'text-2xl',
   },
   {
     label: 'Heroku',
-    proficiency: '80%',
+    proficiency: '50%',
     icon: 'logos:heroku-icon',
     iconClasses: 'text-2xl',
   },
@@ -197,13 +119,11 @@ export const BACKEND_SKILLS = [
 ];
 
 export const FRONTEND_DESCRIPTIONS = [
-  'Building complex, interactive user interfaces using React js.',
-  'Building Large scale Web applications using state management library such as redux.',
-  'Building responsive Web Applications.',
+  'Creación de interfaces de usuario complejas e interactivas utilizando React js.',
+  'Creación de aplicaciones web responsivas.',
 ];
 
 export const BACKEND_DESCRIPTIONS = [
-  'Building RESTful APIs and GraphQL APIs.',
-  'Worked with various Node.js modules and libraries, such as Express to develop full-stack applications.',
-  'good understanding of database design in technologies such as PostgreSQL and MySQL.',
+  'Trabajé con varios módulos y bibliotecas de Node.js, como Express para desarrollar aplicaciones full-stack.',
+  'Buen conocimiento del diseño de bases de datos en tecnologías como MongoDB y MySQL.',
 ];
